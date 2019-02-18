78007ec685cdc584d9816096d3886c6d6c55f826
Nate <nate998877@gmail.com>
2019-02-18 02:05:35 -0500

    started implementing sqlite db for enphase historic data

---
+++ Importer/enphaseDB.py
@@ -0,0 +1,29 @@
+import requests
+import sqlite3
+import os
+
+# Method calls Enphase's (Cottage Rooftop) Array and returns a summary of the system
+def get_enphase(api_base, param):
+    resp = requests.get(api_base, params=param)
+    d = dict(resp.json())
+    system_id = d['systems'][1]['system_id']
+    resp = requests.get('{0}/{1}/summary'.format(api_base, system_id), params=param)
+    return dict(resp.json())
+
+
+def initalize(db_path):
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute("""CREATE DATABASE IF NOT EXISTS enphase""")
+    c.execute("""CREATE TABLE IF NOT EXISTS historicPower (date, time, power)""")
+    return c;
+
+
+def add(c, values):
+    insert = """INSERT INTO historicPower (date, time, power) values({0}, {1}, {2})"""
+    c.execute(insert.format(values[0], values[1], values[2]))
+
+
+def call(c):
+    print(c)
+    print(c.execute("""SELECT * FROM *"""))
